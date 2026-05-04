9ef8c5e5c670fe0ccb6acc9def0bfb6afcb85bf2
blueheart.elf <arislam.cs28@gmail.com>
2026-05-04 14:17:05 -0400

    Created VEX Project

---
+++ islam-vex/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       arish                                                        #
+# 	Created:      5/4/2026, 2:15:03 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
