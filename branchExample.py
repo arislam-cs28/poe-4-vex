369249fb43c219503e3bf8d8c1f6d32b938b7111
blueheart.elf <arislam.cs28@gmail.com>
2026-05-04 13:54:06 -0400

    Create branchExample.py

---
+++ branchExample.py
@@ -0,0 +1,12 @@
+"""
+Name:           branchExample.py
+Author(s):      Arisha Islam & Amy Wang
+Description:    Demonstrate how to use GitHub branches
+Date:           5/4/26
+"""
+
+counter = 0
+
+while(counter < 10):
+    print("Time: " + str(counter))
+    counter += 1
